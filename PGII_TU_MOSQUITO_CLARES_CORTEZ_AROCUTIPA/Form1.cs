995f490a24efa2b91ed7a9bd9fde9604c5f250b5
Luis David <lm2023077088@virtual.upt.pe>
2025-09-06 11:06:11 -0500

    Creacion de Archivo

---
+++ PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA
+{
+    public partial class Form1 : Form
+    {
+        //Declaracion de variables globales
+        string strUsuario = "Admin";
+        string strPassword = "1234";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnacceder_Click(object sender, EventArgs e)
+        {
+            if (txtusuario.Text.ToUpper() == strUsuario.ToUpper() && txtpassword.Text == strPassword)
+            {
+                string a = txtusuario.Text;
+                string b = txtpassword.Text;
+
+                MDIParent1 mDI_Principal = new MDIParent1();
+                mDI_Principal.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Error, Datos incorrectos");
+            }
+        }
+    }
+}
---
+++ PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA/MDIParent1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA
+{
+    partial class MDIParent1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.eJERCICIOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eJERCICIO01ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eJERCICIOSToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(347, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // eJERCICIOSToolStripMenuItem
+            // 
+            this.eJERCICIOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eJERCICIO01ToolStripMenuItem});
+            this.eJERCICIOSToolStripMenuItem.Name = "eJERCICIOSToolStripMenuItem";
+            this.eJERCICIOSToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.eJERCICIOSToolStripMenuItem.Text = "EJERCICIOS";
+            // 
+            // eJERCICIO01ToolStripMenuItem
+            // 
+            this.eJERCICIO01ToolStripMenuItem.Name = "eJERCICIO01ToolStripMenuItem";
+            this.eJERCICIO01ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eJERCICIO01ToolStripMenuItem.Text = "EJERCICIO 01";
+            // 
+            // MDIParent1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 264);
+            this.Controls.Add(this.menuStrip);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MDIParent1";
+            this.Text = "MDIParent1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ToolStripMenuItem eJERCICIOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eJERCICIO01ToolStripMenuItem;
+    }
+}
+
+
+
