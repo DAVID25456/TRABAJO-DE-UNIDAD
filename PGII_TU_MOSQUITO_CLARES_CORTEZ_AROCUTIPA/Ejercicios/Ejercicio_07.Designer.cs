db24a44b0ecd388e36a9f2c3b0bb32c65a86f540
JulioCortez9495 <jc2023077283@gmail.com>
2025-09-06 21:38:30 -0500

    CAMBIOS SAMUEL

--- PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA/Ejercicios/Ejercicio_07.Designer.cs
+++ PGII_TU_MOSQUITO_CLARES_CORTEZ_AROCUTIPA/Ejercicios/Ejercicio_07.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(389, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Ejercicio_07
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Ejercicio_07";
+            this.Controls.Add(this.button1);
+            this.Name = "Ejercicio_07";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
